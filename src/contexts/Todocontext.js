292cf2810cbfb06c8cbf707d98d40c38a4f205e2
itzsaad8 <msikhan32@gmail.com>
2024-03-12 16:30:09 +0500

    first

---
+++ src/contexts/Todocontext.js
@@ -0,0 +1,24 @@
+import { useContext ,createContext } from "react";
+
+
+export const Todocontext = useContext({
+    todos: [
+        {
+            id: 1,
+            todo: 'todo msg',
+            completed: false
+        }
+    ],
+    addtodo: (todo) => {},
+    updatetodo: (id,todo ) => {},
+    deletetodo: (id) => {},
+    togglecomplete: (id) => {},
+})
+
+
+
+export const useTodo= () =>{
+    return useContext(Todocontext)
+}
+
+export const TodoProvider = Todocontext.Provider
